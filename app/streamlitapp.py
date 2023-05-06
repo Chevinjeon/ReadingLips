4c41a345a3d9dd79d4f0d867d5f09ece79b3f5c0
Chevin Jeon <chevinjeon@gmail.com>
2023-05-05 20:25:48 -0400

    initial commit

--- app/streamlitapp.py
+++ app/streamlitapp.py
@@ -15,7 +15,7 @@ st.set_page_config(layout='wide')
 # Set up the sidebar
 with st.sidebar: 
     st.image('https://www.onepointltd.com/wp-content/uploads/2020/03/inno2.png')
-    st.title('LipBuddy')
+    st.title('Lip Reading')
     st.info('This application is originally developed from the LipNet deep learning model.')
 
 
